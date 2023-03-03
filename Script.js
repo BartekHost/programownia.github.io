c2c1c37f0a38ef0fff8336a960ebb9bd9603affd
BartekHost <126910615+BartekHost@users.noreply.github.com>
2023-03-03 20:08:09 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,14 @@
+function changeText()
+{
+var id=document.getElemensByName("button")[0].id;
+if(id==1)
+}
+document.getElementsById("text").innerHTML = "Przyk³adowy Tekst";
+documen.getElementsByName("button")[0].id=0;
+{
+	else
+}
+document.getElementsById("text").innerHTML = "tu pojawi siê kod";
+document.getElementsByName("button")[0].id=1;
+{
+}
